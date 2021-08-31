740622cb53ae344b6d2f41e99c826e6e0f983064
CarlosC074 <carlos.castano74@gmail.com>
2021-08-30 17:42:14 -0700

    Added code to: search function, current report window, and each search generates a 'previously searched' tab which is clickable, to align with the acceptance criteria. TODO: have the call insert information to the forecast tabs

--- assets/script/script.js
+++ assets/script/script.js
@@ -2,7 +2,7 @@
 var cityName = document.getElementById("cityName");
 var submitBtn = document.getElementById("submitBtn");
 //all previous results will generate with a prevSearch class
-var prevSearches = document.getElementsByClassName("prevSearch");
+var prevSearches = document.getElementById("prevSearchWindow");
 var todayWindow = document.getElementById("todayCast");
 var forecastCards = document.getElementsByClassName("forecastCard"); 
 var d = new Date;
@@ -44,16 +44,57 @@ function fetchOneCall(x,y) {
         })
         .then(function(oneCall) {
             console.log(oneCall);
+            console.log(oneCall.daily[2].temp.day)
             //Current report window
+                //city name, date
             const namedCity =  cityName.value.charAt(0).toUpperCase() + cityName.value.slice(1);
-            namedCityTitle = document.getElementsByTagName("h1");
+            const namedCityTitle = document.getElementsByTagName("h1");
             namedCityTitle[0].innerHTML = `${namedCity}  ${date}`;
-            //
+                //temperature
+            const currentTemp = Math.floor(oneCall.current.temp);
+            const currentTempTab = document.getElementById("currentTemp");
+            currentTempTab.innerHTML = `Temp: ${currentTemp}°F`;
+                //wind speed
+            const currentWind = oneCall.current.wind_speed;
+            const currentWindTab = document.getElementById("currentWind");
+            currentWindTab.innerHTML = `Wind(mp/h): ${currentWind} mp/h`;
+                //humidity
+            const currentHumidity = oneCall.current.humidity;
+            const currentHumidityTab = document.getElementById("currentHumidity");
+            currentHumidityTab.innerHTML = `Humidity: ${currentHumidity}%`;
+                //uvi index
+            const currentUVI = Math.floor(oneCall.current.uvi);
+            const currentUVITab = document.getElementById("currentUVI");
+            currentUVITab.innerHTML = `UV index: ${currentUVI}`
+            currentUVITab.classList.remove("uviSafe");
+            currentUVITab.classList.remove("uviWarning");
+            currentUVITab.classList.remove("uviDanger");
+                if(currentUVI < 6){
+                    currentUVITab.classList.add("uviSafe");
+                }
+                else if(currentUVI >= 6){
+                    currentUVITab.classList.add("uviWarning");
+                }
+                else {
+                    currentUVITab.classList.add("uviDanger");
+                }
+            //previous searches generator
+            const prevSearchTab = document.createElement("div");
+            prevSearchTab.innerHTML = namedCity;
+            prevSearchTab.classList.add("prevSearch");
+            prevSearchTab.addEventListener("click", function() {
+                cityName.value = namedCity;
+                fetchAllInfo();
+                this.remove();
+            });
+            prevSearches.appendChild(prevSearchTab);
+            //5 day forecast
+            for (i = 0; i < forecastCards.length; i++){
+                const cardDate = d.addDays(i + 1).toLocaleDateString("en-US");
+                cons
 
 
-            
-            
-
+            }
 
         })
 }
